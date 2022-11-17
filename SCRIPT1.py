161ca755a7f01523c601c55383a88d6048fab3f1
OrangeStealer <118409517+OrangeStealer@users.noreply.github.com>
2022-11-16 19:12:00 -0700

    11/16/22 7:11PM

--- SCRIPT1.py
+++ SCRIPT1.py
@@ -1,1 +1,32 @@
+import time
 
+import keyboard
+from codrone_edu.drone import *
+
+#Inits drone library and pairs to the drone
+drone = Drone()
+#drone.pair(portname)
+drone.pair()
+ 
+#Variables
+battery = drone.get_battery()
+Temp = drone.get_drone_temp()
+
+#print drone status
+print('Temp: ' + str(Temp) + 'C')
+
+#Resets the drones sensor and sleeps for 1 second
+drone.reset_sensor() 
+time.sleep(1)
+
+#If batter is less than 30% then wont start
+if battery <= 30:
+    exit()
+
+#Movement
+drone.takeoff()
+drone.hover(1)
+
+#land and close drone connection
+drone.land()
+drone.close()
