3e0dadf709bcacb4a0b9b60a3d5fce1387421f20
OrangeStealer <118409517+OrangeStealer@users.noreply.github.com>
2022-11-21 11:01:35 -0700

    11/21/22 11:01AM

--- SCRIPT1.py
+++ SCRIPT1.py
@@ -1,5 +1,4 @@
 import time
-
 import keyboard
 from codrone_edu.drone import *
 
@@ -33,35 +32,47 @@ if Temp >= 45:
 
 #127 x 63 pixels
 #create controller screen UI
+drone.set_controller_LED(255,0,0,100)
 drone.controller_clear_screen()
-drone.sendDisplayDrawString(0, 0, "SCRIPT1 V3.2")
+drone.sendDisplayDrawString(0, 0, "SCRIPT1 V3.5")
 time.sleep(2)
-drone.sendDisplayDrawString(0, 0, "LOADING")
-time.sleep(5)
+drone.controller_clear_screen()
+drone.sendDisplayDrawString(0, 0, "LOADING.")
+time.sleep(1)
+drone.sendDisplayDrawString(0,0,"LOADING..")
+time.sleep(1)
+drone.sendDisplayDrawString(0,0,"LOADING...")
+time.sleep(1)
+drone.sendDisplayDrawString(0,0,"LOADING.")
+time.sleep(1)
+drone.sendDisplayDrawString(0,0,"LOADING..")
 
 #get takeoff location data
 takeoffloc = drone.get_position_data()
 print(takeoffloc)
 drone.set_waypoint()
 
+#draw controller screen UI
+drone.controller_clear_screen()
+drone.sendDisplayDrawString(0, 0, "SCRIPT1 V3.5")
+drone.sendDisplayDrawString(0, 10, "Battery: " + str(battery) + "%")
+drone.sendDisplayDrawString(0, 20, "Temp: " + str(Temp) + "C")
+
 #make controller LED green to indicate that drone is ready
 drone.set_controller_LED(21, 162, 12, 100)
-drone.controller_preview_canvas()
+drone.sendDisplayDrawString(0,0,"Press S to start Drone!")
+#drone.controller_preview_canvas(image)
 
 #press S to start 
 while True:
     time.sleep(0.1)
     if drone.s_pressed():
          drone.takeoff()
+         drone.controller_clear_screen()
+         drone.sendDisplayDrawString(0,0,"Code Running")
+         break
 
 #Movement
-drone.hover(1)
-drone.keep_distance(10, 600)
-fcd1 = drone.get_front_color()
-print(fcd1)
-
-#return home if red block in front of drone
-#else move to right and loop
 def FCN1():
     if fcd1 == red:
         drone.goto_waypoint(drone.waypoint_data[0], 0.5)
